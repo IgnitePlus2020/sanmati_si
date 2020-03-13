bf463294d27b4f368d550cc6ace2f2e200284f71
sanmati-rm <sanmati-rm@users.noreply.github.com>
2020-03-13 22:25:34 +0530

    done

---
+++ src/si/si.java
@@ -0,0 +1,19 @@
+package si;
+
+import java.util.Scanner;
+
+public class si {
+    public static void main(String[] args) {
+        float principal=0,rate=0,time=0;
+        float si=0;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the principal amount\n");
+        principal=in.nextFloat();
+        System.out.println("Enter the rate of interest\n");
+        rate=in.nextFloat();
+        System.out.println("Enter the time period in months\n");
+        time=in.nextFloat();
+        si=(principal*rate*time)/100;
+        System.out.println("Simple interest is" + si);
+    }
+}
